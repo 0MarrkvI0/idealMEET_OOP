f51563514b51f06167c5236594384120843500e9
Martin Kvietok <xkvietokm@stuba.sk>
2024-05-12 23:48:26 +0200

    verzia

--- src/MeetingSchedule/NormMeeting.java
+++
@@ -1,9 +0,0 @@
-package MeetingSchedule;
-
-import java.time.LocalDate;
-import java.util.Date;
-public class NormMeeting extends Meeting {
-    public NormMeeting(LocalDate date, int StartTime, int Duration, int capacity, String RoomID) {
-        super(date, StartTime, Duration, capacity, RoomID);
-    }
-}
